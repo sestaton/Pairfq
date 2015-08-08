b87644af32b916e059eed49a763bba1701f9606f
sestaton <statonse@gmail.com>
2015-08-08 04:30:53 -0700

    bump version

--- scripts/pairfq_lite.pl
+++ scripts/pairfq_lite.pl
@@ -7,7 +7,7 @@ use File::Basename;
 use Getopt::Long;
 use Pod::Usage;
 
-our $VERSION = '0.14.4';
+our $VERSION = '0.14.5';
 
 my $infile;     # input file for 'addinfo', 'splitpairs' and 'makepairs' methods
 my $outfile;    # output file for 'addinfo' method
